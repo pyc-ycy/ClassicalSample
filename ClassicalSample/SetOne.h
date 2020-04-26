fd2896661e2f95a2ea1f8e13df7e35b3b2fb9b28
7wc98#14 <2923616405@qq.com>
2020-04-26 21:52:43 +0800

    Update

--- ClassicalSample/SetOne.h
+++ ClassicalSample/SetOne.h
@@ -92,3 +92,16 @@ void example5()
 	}
 	printf("three number order from min to max:%d %d %d", x, y, z);
 }
+/*
+	题目：用*号输出字母C的图案。
+
+程序分析：可先用'*'号在纸上写出字母C，再分行输出。
+*/
+void example6()
+{
+	printf("use * print a symbol C\n");
+	printf(" ****\n");
+	printf(" *\n");
+	printf(" * \n");
+	printf(" ****\n");
+}
